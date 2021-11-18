717dbbb75959b332b0f0de7850ac1c55de6127de
Krecik223 <michal223300@gmail.com>
2021-11-18 16:54:26 +0100

    Further layout work

--- Source code/MainWindow.xaml.cs
+++ Source code/MainWindow.xaml.cs
@@ -32,19 +32,66 @@ namespace Interpreter_ATARI_Logo
             bool output = showOutput.IsChecked;
             bool editor = showEditor.IsChecked;
 
-            if (output || editor)
+            bottomOverlay.Visibility = Visibility.Collapsed;
+            overlaySplitter.Visibility = Visibility.Collapsed;
+
+            editorCanv.Visibility = Visibility.Collapsed;
+            outputCanv.Visibility = Visibility.Collapsed;
+
+            if (editor)
             {
                 bottomOverlay.Visibility = Visibility.Visible;
+                editorCanv.Visibility = Visibility.Visible;
+
+                if (output)
+                {
+                    overlaySplitter.Visibility = Visibility.Visible;
+
+                    editorCanv.SetValue(Grid.ColumnSpanProperty, 1);
+                    outputCanv.SetValue(Grid.ColumnSpanProperty, 1);
+                    outputCanv.SetValue(Grid.ColumnProperty, 2);
+                }
+                else
+                {
+                    editorCanv.SetValue(Grid.ColumnSpanProperty, 3);
+                    editorCanv.SetValue(Grid.ColumnProperty, 0);
+                }
             }
-            else
+
+            if (output)
             {
-                bottomOverlay.Visibility = Visibility.Collapsed;
+                bottomOverlay.Visibility = Visibility.Visible;
+                outputCanv.Visibility = Visibility.Visible;
+
+                if (!editor)
+                {
+                    outputCanv.SetValue(Grid.ColumnSpanProperty, 3);
+                    outputCanv.SetValue(Grid.ColumnProperty, 0);
+                }
             }
         }
 
         private void CloseWindow(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private void CloseTab(object sender, RoutedEventArgs e)
+        {
+            string value = ((Button)sender).Tag.ToString();
+
+            switch (value)
+            {
+                case "editor":
+                    showEditor.IsChecked = false;
+                    UpdateBottomOverlay();
+                    break;
+
+                case "output":
+                    showOutput.IsChecked = false;
+                    UpdateBottomOverlay();
+                    break;
+            }
         }
     }
 }
